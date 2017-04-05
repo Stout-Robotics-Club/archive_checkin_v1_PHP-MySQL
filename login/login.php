945cb657f5cd3651a39ba601ee15c7cbbf5e3b2f
becherj0489 <becherj0489@my.uwstout.edu>
2017-04-05 15:42:23 -0500

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,1 +1,17 @@
-//file for logins
+<html>
+	
+	<?php
+		
+		$username = "username";
+		$password = "password";
+		$dbname = "database";
+	
+		//create connection
+		$conn = mysqli_connect($SERVER_NAME, $username, $password, $dbname);
+	
+		//check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());	
+		}
+	?>
+</html>
