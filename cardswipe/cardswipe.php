162dc7b29452e2665b0d16c7a9e7c8a0ed5f842d
Michelle Miller <chellimiller@users.noreply.github.com>
2017-04-05 14:40:04 -0500

    Create cardswipe.php

---
+++ cardswipe/cardswipe.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Card Swipe</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id="content">
+			<h1>Please Swipe Card</h1>
+			<form action="swiped.php" method="post">
+				<input type="number" name="id"></input>
+				<input type="submit">Submit</input> 
+			</form>
+		</div>
+	</body>
+	
+</html>
