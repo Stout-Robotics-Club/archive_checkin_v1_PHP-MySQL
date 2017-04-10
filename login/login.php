856cfdee2e09ceac68b4989379d7d68d0666fc73
becherj0489 <becherj0489@my.uwstout.edu>
2017-04-10 15:05:37 -0500

    Update login.php

--- login/login.php
+++ login/login.php
@@ -5,14 +5,17 @@
 		$username = "username";
 		$password = "password";
 		$dbname = "database";
-		$server = $_SERVER['SERVER_NAME];
+		//$server = $_SERVER['SERVER_NAME];
 	
 		//create connection
-		$conn = mysqli_connect($SERVER_NAME, $username, $password, $dbname);
+		//$conn = mysqli_connect($SERVER_NAME, $username, $password, $dbname);
+		$conn = mysqli_connect($username, $password, $dbname);
+		
 	
 		//check connection
 		if (!$conn) {
 			die("Connection failed: " . mysqli_connect_error());	
 		}
+		echo "Connected successfully";
 	?>
 </html>
