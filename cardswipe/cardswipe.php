947aca275d824eb59824e60e87b237cdbd0bf502
njsoly <njsoly@polymuler.com>
2017-04-12 13:27:35 -0500

    made text input field 'autofocus'.

--- cardswipe/cardswipe.php
+++ cardswipe/cardswipe.php
@@ -8,7 +8,7 @@
 		<div id="content">
 			<h1>Please Swipe Card</h1>
 			<form action="swiped.php" method="post">
-				<input type="number" name="id"></input>
+				<input type="number" name="id" autofocus></input>
 				<input type="submit">Submit</input> 
 			</form>
 		</div>
